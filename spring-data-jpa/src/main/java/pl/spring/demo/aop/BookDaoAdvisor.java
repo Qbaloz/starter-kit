84bbc058ab00f1b4351665ce698478ec1a9e72a0
Jakub Wawrzyniak <qbaloz1991@gmail.com>
2016-01-12 16:06:38 +0100

    Beans configuration changed from XML to annotation.

--- spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/aop/BookDaoAdvisor.java
@@ -1,35 +1,20 @@
 package pl.spring.demo.aop;
 
 
-import org.springframework.aop.MethodBeforeAdvice;
-import pl.spring.demo.annotation.NullableId;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 import pl.spring.demo.exception.BookNotNullIdException;
 import pl.spring.demo.to.IdAware;
 
-import java.lang.reflect.Method;
+@Aspect
+@Component
+public class BookDaoAdvisor {
 
-public class BookDaoAdvisor implements MethodBeforeAdvice {
-
-    @Override
-    public void before(Method method, Object[] objects, Object o) throws Throwable {
-
-        if (hasAnnotation(method, o, NullableId.class)) {
-            checkNotNullId(objects[0]);
-        }
-    }
-
-    private void checkNotNullId(Object o) {
-        if (o instanceof IdAware && ((IdAware) o).getId() != null) {
-            throw new BookNotNullIdException();
-        }
-    }
-
-    private boolean hasAnnotation (Method method, Object o, Class annotationClazz) throws NoSuchMethodException {
-        boolean hasAnnotation = method.getAnnotation(annotationClazz) != null;
-
-        if (!hasAnnotation && o != null) {
-            hasAnnotation = o.getClass().getMethod(method.getName(), method.getParameterTypes()).getAnnotation(annotationClazz) != null;
-        }
-        return hasAnnotation;
-    }
+	@Before("@annotation(pl.spring.demo.annotation.NullableId) && args(idAware,..)")
+	private void checkNotNullId(IdAware idAware){
+		if(idAware.getId() != null){
+			throw new BookNotNullIdException();
+		}
+	}
 }
